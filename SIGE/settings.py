f575f76d0fdec4e09b6df21bf9c362bb9e6e2b46
Jorge Junio <jorgejunioufrn@gmail.com>
2021-02-04 19:38:40 -0300

    Alterações na ordem da definição das Apps instaladas no projeto.

--- SIGE/settings.py
+++ SIGE/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['srale.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'escolas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'escolas',
     'bootstrapform'
 ]
 
